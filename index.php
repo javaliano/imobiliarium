055ed4ba8b1813705e5e0bec42f89f7084640f0b
javaliano <javalianocarniel@gmail.com>
2022-06-19 00:52:33 -0300

    co it 00:52 19/06/22

--- index.php
+++ index.php
@@ -4,18 +4,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+@import url('https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Anton&family=Bebas+Neue&family=Berkshire+Swash&family=Blaka&family=Fascinate+Inline&family=Fira+Sans&family=Kaushan+Script&family=Orbitron&family=Oswald&family=Source+Serif+Pro&display=swap');
+</style> 
     <title>Imobiliarium</title>
-    <link rel="stylesheet" href="css/style-index.css">
+    <!-- <link rel="stylesheet" href="css/style-index.css"> -->
+    <link rel="stylesheet" href="node_modules/bootstrap/javaliano-compilados/bootstrap.css" >
 </head>
 <body>
-<div id="body-form">
-<h1>Entrar</h1>
-<form action="hub.php" method="POST">
-    <input type="text" placeholder="usuário">
-    <input type="password" placeholder="senha">
-    <input type="submit" value="Cadastrar"> <!--Cadastrar com C maiusculo-->
-    <a href="cadastrar.php"><strong>Ainda não possui cadastro? Cadastre-se aqui.</strong></a>
+    <script src="js/jquery.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+    <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
+<div id="body-form" class="body-form">
+    <br><br>
 
+<table align="center">
+    <tr>
+    <td><h1 class="display-1" id="h1-1">Imobiliarium<br></h1></td>
+    
+    </tr>
+   
+</table>
+    
+<br>
+<h4>login:</h4>
+<form action="hub.php" method="POST">
+    <label><input type="text" placeholder="usuário"></label>
+    <label> <input type="password" placeholder="senha"></label><br><br>
+    <label><input id="id-inputsubmit" type="submit" value="Entrar"> <!--Cadastrar com C maiusculo--></label>
+   
+    
+    <a href="cadastrar.php"><strong>Não possui cadastro? Cadastre</strong></a>
+    <br><br><br>
 </form>
 </div>
     
---
+++ indexBKP.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+@import url('https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Anton&family=Bebas+Neue&family=Fascinate+Inline&family=Fira+Sans&family=Orbitron&family=Oswald&family=Source+Serif+Pro&display=swap');
+</style> 
+    <title>Imobiliarium</title>
+    <!-- <link rel="stylesheet" href="css/style-index.css"> -->
+    <link rel="stylesheet" href="node_modules/bootstrap/javaliano-compilados/bootstrap.css" >
+</head>
+<body>
+    <script src="js/jquery.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+    <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
+<div id="body-form" class="body-form">
+    <br><br>
+
+
+    <h1 class="display-1" id="h1-1">IMOBILIARIUM<br></h1>
+<br><br>
+<h2>login:</h2>
+<form action="hub.php" method="POST">
+    <label><input type="text" placeholder="usuário"></label>
+    <label> <input type="password" placeholder="senha"></label>
+    <label><input type="submit" value="Entrar"> <!--Cadastrar com C maiusculo--></label>
+   
+    
+    <a href="cadastrar.php"><strong>Não possui cadastro? Cadastre</strong></a>
+    <br><br><br>
+</form>
+</div>
+    
+</body>
+</html>
